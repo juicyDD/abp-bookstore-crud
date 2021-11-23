354796e66c9c90ef2b553a60d7bdb7a8a026f10b
juicyDD <ongnguyenuyennhi@gmail.com>
2021-11-23 16:30:02 +0700

    crud

---
+++ src/Acme.BookStore.Web/Pages/Books/CreateModal.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Acme.BookStore.Books;
+
+namespace Acme.BookStore.Web.Pages.Books
+{
+    public class CreateModalModel : BookStorePageModel
+    {
+        public CreateUpdateBookDto Book { get; set; }
+
+
+        private readonly IBookAppService _bookAppService;
+
+        public CreateModalModel(IBookAppService bookAppService)
+        {
+            _bookAppService = bookAppService;
+        }
+
+        public void OnGet()
+        {
+            Book = new CreateUpdateBookDto();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _bookAppService.CreateAsync(Book);
+            return NoContent();
+        }
+    }
+}
